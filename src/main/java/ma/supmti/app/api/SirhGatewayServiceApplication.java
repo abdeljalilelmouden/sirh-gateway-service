ac598207d0dbec4e7e545b03d1cdcece996e3002
ABDELJALIL EL MOUDEN <elmoudene123@gmail.com>
2021-05-08 18:57:01 +0200

    service gateway

--- src/main/java/ma/supmti/app/api/SirhGatewayServiceApplication.java
+++ src/main/java/ma/supmti/app/api/SirhGatewayServiceApplication.java
@@ -3,15 +3,25 @@ package ma.supmti.app.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableEurekaClient
+@CrossOrigin(origins = "http://localhost:4500")
 public class SirhGatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SirhGatewayServiceApplication.class, args);
 	}
-
+	@Bean
+	DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp) {
+		return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
+	}
+ 
 }
